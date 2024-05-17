018f16df13516546b2e9bbcd9679da9c2916ee41
weJee <1243087518@qq.com>
2024-05-17 17:00:02 +0800

    feat: 两数相加

--- 2-两数相加.js
+++ 2-两数相加.js
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -16,16 +15,29 @@ const addTwoNumbers = (l1, l2) => {
   let head = null;
   let tail = null;
   let carry = 0;
-  
-  while(l1 || l2) {
-    const n1 = l1 ? l1.val : 0
-    const n2 = l2 ? l2.val : 0
-    const sum = n1 + n2 + carry
+
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0;
+    const n2 = l2 ? l2.val : 0;
+    const sum = n1 + n2 + carry;
     if (!head) {
-       head = tail = new ListNode(sum % 10)
+      head = tail = new ListNode(sum % 10);
     } else {
-       tail.next = new ListNode(sum % 10)
-       tail = tail.next
+      tail.next = new ListNode(sum % 10);
+      tail = tail.next;
+    }
+
+    carry = Math.floor(sum / 10);
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
     }
   }
+
+  if (carry > 0) {
+    tail.next = new ListNode(carry)
+  }
+  return head
 };
